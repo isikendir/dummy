beeacafdd69de552fcb8b1f4aac11d0475e9ddbf
isikendir <isik.endir@gmail.com>
2025-11-19 10:17:57 +0300

    test icin

---
+++ myTestScript2.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+APPIUM_PORT=4723
+APPIUM_PID=0
+
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+echo "Version "1"  Script started at: $(date '+%m/%d %H:%M:%S')"
+echo "xxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+echo "........"
